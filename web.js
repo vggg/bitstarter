184ae1c8191567c416fb9142ed5c78fe45f8e8f8
Vikram Godbole <vikram.godbole@shalkiengineers.com>
2013-07-17 14:11:22 +0000

    Updated web.js to read from index.html

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buffer = new Buffer(fs.readFileSync('index.html','utf-8');
+  var buffer = new Buffer(fs.readFileSync('index.html'),'utf-8');
   response.send(buffer.toString());
 });
 
