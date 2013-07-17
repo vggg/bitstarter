bc7e9094f611eeb86d4a1d84fd4f2c6f0898709d
Vikram Godbole <vikram.godbole@shalkiengineers.com>
2013-07-17 14:06:10 +0000

    Updated web.js to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var buffer = new Buffer(fs.readFileSync('index.html','utf-8');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
